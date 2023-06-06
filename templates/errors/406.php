be9ddbcd34e5a3b4754164067c37f01e3a0bb6b1
celionatti <amisuusman@gmail.com>
2023-06-06 09:15:06 -0700

    error fixes

--- templates/errors/406.php
+++ templates/errors/406.php
@@ -8,9 +8,8 @@ use Core\Config;
 <?php $this->start('content') ?>
 <div class="container py-5 d-flex flex-column align-items-center justify-content-center shadow">
     <div class="py-5">
-        <img src="<?= get_image("assets/img/404.svg") ?>" class="" />
-
-        <a href="<?= Config::get("domain") ?>" class="bi bi-arrow-left-circle px-2 btn btn-sm btn-primary w-100"> Back</a>
+        <h5 class="text-danger text-uppercase text-center border-bottom border-danger border-3">Not acceptable Error</h5>
+        <img src="<?= get_image("assets/img/error-1.jpg") ?>" class="img-thumbnail" style="height:460px;" />
     </div>
 </div>
 <?php $this->end(); ?>
